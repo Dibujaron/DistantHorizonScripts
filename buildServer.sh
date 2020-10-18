2f1c699a593738a624d8d5e85665881688d9f9f2
Cloud User <centos@ns520612.ip-158-69-120.net>
2020-10-18 23:23:26 +0000

    Make server build also update world and shipclasses

--- buildServer.sh
+++ buildServer.sh
@@ -1,5 +1,7 @@
 rm -rf ~/server/*
 ~/scripts/makeWarning.sh
 cp ~/repos/DistantHorizonServer/out/artifacts/DistantHorizonServer_jar/DistantHorizonServer.jar ~/server/DistantHorizonServer.jar
+cp -r ~/repos/DistantHorizonServer/shipclasses/ ~/server
+cp -r ~/repos/DistantHorizonServer/world/ ~/server
 cp ~/config/server.properties ~/server/server.properties
 echo --server built--
--- buildWeb.sh
+++ buildWeb.sh
@@ -3,9 +3,6 @@ cd ~/web
 ~/scripts/makeWarning.sh
 cp -r ~/repos/DistantHorizonBalancer/* ~/web/
 cp ~/config/balancer.ini ~/web/config.ini
-mkdir client
-cd ~/web/client
-~/scripts/makeWarning.sh
 cd ~/repos/DistantHorizonClient
-godot --path ~/repos/DistantHorizonClient/project.godot --export Distant-Horizon ~/web/client/Distant-Horizon.html
+godot --path ~/repos/DistantHorizonClient/project.godot --export Distant-Horizon ~/web/Distant-Horizon.html
 echo --web built--
