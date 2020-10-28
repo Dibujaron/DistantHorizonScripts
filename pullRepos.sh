a4193cef50451fd2186a54f6ebd8d0c86c6fd573
Cloud User <centos@ns520612.ip-158-69-120.net>
2020-10-28 02:38:51 +0000

    add resets to build script

--- pullRepos.sh
+++ pullRepos.sh
@@ -1,9 +1,13 @@
 cd ~/repos/DistantHorizonScripts
+git reset --hard
 git pull
 cd ~/repos/DistantHorizonServer
+git reset --hard
 git pull
 cd ~/repos/DistantHorizonClient
+git reset --hard
 git pull
 cd ~/repos/DistantHorizonBalancer
+git reset --hard
 git pull
 echo --Repositories updated--
