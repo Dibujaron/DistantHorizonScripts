eca5a95c97af1feacb805397d3f43ac63a1e478d
Cloud User <centos@ns520612.ip-158-69-120.net>
2020-11-09 04:22:31 +0000

    buildDates2

--- buildWeb.sh
+++ buildWeb.sh
@@ -1,10 +1,11 @@
+~/scripts/updateBuildDate.sh
 rm -rf ~/web/*
 cd ~/web
 ~/scripts/makeWarning.sh
 cp -r ~/repos/DistantHorizonBalancer/* ~/web/
 ~/scripts/removeGithubFiles.sh
 cp ~/config/balancer.ini ~/web/config.ini
+cp ~/config/lastbuild.txt ~/web/lastbuild.txt
 cd ~/repos/DistantHorizonClient
 godot --path ~/repos/DistantHorizonClient/project.godot --export Distant-Horizon ~/web/Distant-Horizon.html
-~/scripts/updateBuildDate.sh
 echo --web built--
