e4b5a4a8eb2a3c0e60f6ed69813aa5a77274c463
Cloud User <centos@ns520612.ip-158-69-120.net>
2020-11-21 20:31:38 +0000

    addHomeDirToGunicornOutput

--- deployGunicorn.sh
+++ deployGunicorn.sh
@@ -1,1 +1,1 @@
-authbind /usr/local/bin/gunicorn -b 0.0.0.0:80 app:app --error-logfile ~/logs/gunicorn/error.log --access-logfile /logs/gunicorn/access.log --capture-output --log-level debug
+authbind /usr/local/bin/gunicorn -b 0.0.0.0:80 app:app --error-logfile ~/logs/gunicorn/error.log --access-logfile ~/logs/gunicorn/access.log --capture-output --log-level debug
