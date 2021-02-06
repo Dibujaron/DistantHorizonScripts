4d4b269222337b2baa5ef8bfcbd9df718cce6115
Cloud User <centos@ns520612.ip-158-69-120.net>
2021-02-06 20:40:14 +0000

    addLogging

--- runServerInternal.sh
+++ runServerInternal.sh
@@ -1,2 +1,3 @@
 cd ~/server/
-java -Xmx4096m -Xms1024m -jar DistantHorizonServer.jar
+rm ~/server/log/log_latest.txt
+java -Xmx4096m -Xms1024m -jar DistantHorizonServer.jar > ~/server/log/log_latest.txt 2>&1
